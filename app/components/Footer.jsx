a53ea58ec7be6b561f6006d133756532ffc0d8d4
Muhammad Fikri Rouzan Ash Shidik <fikrirzn@gmail.com>
2025-01-05 23:53:57 +0700

    fifth commit

---
+++ app/components/Footer.jsx
@@ -0,0 +1,34 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-20">
+      <div className="text-center">
+        <Image src={assets.logo} alt="" className="w-36 mx-auto mb-2" />
+        <div className="w-max flex items-center gap-2 mx-auto">
+          <Image src={assets.mail_icon} alt="" className="w-6" />
+          fikrirzn@gmail.com
+        </div>
+      </div>
+      <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
+        <p>© {new Date().getFullYear()} Muhammad Fikri Rouzan Ash Shidik</p>
+        <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
+          <li>
+            <a target="_blank" href="https://github.com/Fikri-Rouzan">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://www.linkedin.com/in/fikrirouzan/">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
