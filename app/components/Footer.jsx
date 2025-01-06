39049ae67fd01a009887eda6f0de9a7a4ea75c46
Muhammad Fikri Rouzan Ash Shidik <fikrirzn@gmail.com>
2025-01-06 23:05:00 +0700

    ninth commit

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -11,26 +11,42 @@ const Footer = ({ isDarkMode }) => {
           alt=""
           className="w-36 mx-auto mb-2"
         />
-        <div className="w-max flex items-center gap-2 mx-auto">
+        <a
+          target="_blank"
+          href="https://mail.google.com/mail/?view=cm&to=fikrirzn@gmail.com"
+          className="w-max flex items-center gap-2 mx-auto hover:text-pink-400 dark:hover:text-lime-300 transition"
+        >
           <Image
             src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon}
             alt=""
             className="w-6"
           />
           fikrirzn@gmail.com
-        </div>
+        </a>
       </div>
       <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
         <p>© {new Date().getFullYear()} Muhammad Fikri Rouzan Ash Shidik</p>
         <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
           <li>
             <a target="_blank" href="https://github.com/Fikri-Rouzan">
-              GitHub
+              <Image
+                src={isDarkMode ? assets.github_dark : assets.github}
+                alt=""
+                className="w-6"
+              />
             </a>
           </li>
           <li>
-            <a target="_blank" href="https://www.linkedin.com/in/fikrirouzan/">
-              LinkedIn
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/fikrirouzan/"
+              className="hover:text-pink-400 dark:hover:text-lime-300 transition"
+            >
+              <Image
+                src={isDarkMode ? assets.linkedin_dark : assets.linkedin}
+                alt=""
+                className="w-6"
+              />
             </a>
           </li>
         </ul>
