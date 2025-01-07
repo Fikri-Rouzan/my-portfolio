5d0cd1563c102d3da345d5c5c6474eb7812e391f
Muhammad Fikri Rouzan Ash Shidik <fikrirzn@gmail.com>
2025-01-07 14:35:57 +0700

    first commit

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React from "react";
+import { motion } from "motion/react";
 
 const Footer = ({ isDarkMode }) => {
   return (
@@ -25,7 +26,9 @@ const Footer = ({ isDarkMode }) => {
         </a>
       </div>
       <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
-        <p>© {new Date().getFullYear()} Muhammad Fikri Rouzan Ash Shidik</p>
+        <p className="cursor-pointer dark:hover:text-lime-300 transition">
+          © {new Date().getFullYear()} Muhammad Fikri Rouzan Ash Shidik
+        </p>
         <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
           <li>
             <a target="_blank" href="https://github.com/Fikri-Rouzan">
@@ -37,11 +40,7 @@ const Footer = ({ isDarkMode }) => {
             </a>
           </li>
           <li>
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/fikrirouzan/"
-              className="hover:text-pink-400 dark:hover:text-lime-300 transition"
-            >
+            <a target="_blank" href="https://www.linkedin.com/in/fikrirouzan/">
               <Image
                 src={isDarkMode ? assets.linkedin_dark : assets.linkedin}
                 alt=""
